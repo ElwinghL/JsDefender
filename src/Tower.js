a6c1eb63445e8fc5a76adc6117f079d08e07f03d
ElwinghL <clement_1996@hotmail.com>
2025-07-10 18:42:03 +0200

    adding damages

--- src/Tower.js
+++ src/Tower.js
@@ -7,12 +7,18 @@ class Tower {
   properties() {
     this.firerate = 1 + this.lvl / 10;
     this.range = 100 + 10 * this.lvl;
-    this.bonce = 0;
+    this.bounce = 0;
+    this.damage = 10 + this.lvl;
   }
 
   upgrades() {
     ++this.lvl;
     this.properties();
+  }
+
+  attack(target, ...nextTargets) {
+    target.life -= this.damage;
+    while (this.bounce > 0) this.attack(nextTargets);
   }
 }
 
@@ -20,6 +26,7 @@ class ElectricTower extends Tower {
   properties() {
     this.firerate = 1 + this.lvl / 10;
     this.range = 70 + 10 * this.lvl;
-    this.bonce = this.lvl;
+    this.bounce = this.lvl;
+    this.damage = 5 + this.lvl;
   }
 }
